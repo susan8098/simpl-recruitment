c4c1b5899709352cd4fdf1028d07a0d537de5685
Susan <susan8098@gmail.com>
2020-09-11 22:30:09 -0400

    coming soon landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <Nuxt />
 </template>
 
 <style>
+
+@font-face {
+  font-family: 'Libre Baskerville';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('~assets/fonts/LibreBaskerville-Regular.ttf') format('truetype');
+}
+
 html {
   font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
+    'Libre Baskerville'
     sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -22,6 +23,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  font-size: 62.5%;
 }
 
 *,
@@ -31,32 +33,9 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+body {
+  font-size: 1.8rem;
+  background: #000;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,10 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        simpl
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="p-Index">
+    <Logo />
+    <div class="p-Index_Text">
+      <p>Our site will be launching very soon!</p>
+      <!-- <button>Send us a message at info@findsimplr.com</button> -->
+      <p>In the meantime, send us a message at <a href="mailto:info@findsimplr.com">info@findsimplr.com</a>!</p>
     </div>
   </div>
 </template>
@@ -39,42 +21,45 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+
+.p-Index {
+  background: #062b33;
+  text-align: center;
+  height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  animation: 1s appear;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.p-Index_Text {
+  margin-top: 60px;
+  padding-bottom: 60px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+p {
+  color: #E5E5E5;
+  font-size: 1.8rem;
+  margin: 8px;
+  letter-spacing: .8px;
 }
-
-.links {
-  padding-top: 15px;
+a {
+  color: #BE9A3B;
+}
+button {
+  background: #1E849A;
+  border: none;
+  padding: 15px 30px;
+  color: white;
+  margin: 10px;
+  font-size: 1.5rem;
+  letter-spacing: .8px;
+}
+h1 {
+  font-weight: 400;
 }
 </style>
