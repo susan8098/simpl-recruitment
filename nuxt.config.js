6ef41a15554b403b0303c4ba4bd64733f8e6459d
Susan <susan8098@gmail.com>
2020-09-13 16:27:16 -0400

    manifesto generation fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,96 +1,99 @@
 
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'spa',
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "spa",
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'static',
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "static",
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
-    title: 'Simplr Recruitment',
+    title: "Simplr Recruitment",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Simplr recruitment is a specialized finance and accounting recruitment agency based in Toronto.' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Simplr recruitment is a specialized finance and accounting recruitment agency in Toronto."
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/robots',
-    '@nuxtjs/sitemap'
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/robots",
+    "@nuxtjs/sitemap"
   ],
   pwa: {
     icon: {
-      source: 'static/icon.png'
+      source: "static/icon.png"
+    },
+    manifest: {
+      name: "Simplr Recruitment",
+      lang: "en",
+      description:
+        "Simplr recruitment is a specialized finance and accounting recruitment agency in Toronto.",
+      background_color: '#ffffff'
     }
   },
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {},
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  },
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
   // robots.txt config
   robots: {
-    UserAgent: '*',
-    Disallow: '/admin',
-    Sitemap: 'https://findsimplr.com/sitemap.xml'
+    UserAgent: "*",
+    Disallow: "/admin",
+    Sitemap: "https://findsimplr.com/sitemap.xml"
   },
   // sitemap
   sitemap: {
-    hostname: 'https://findsimplr.com',
-    exclude: [
-      '/admin',
-      '/admin/**'
-    ],
+    hostname: "https://findsimplr.com",
+    exclude: ["/admin", "/admin/**"],
     routes: [
       {
-        url: '/',
+        url: "/",
         priority: 1,
-        lastmod: '2020-09-13'
+        lastmod: "2020-09-13"
       }
     ]
   }
-}
+};
